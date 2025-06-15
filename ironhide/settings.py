5a8985d2c8f68b031357e6f729d3440dc37072df
Lucas Martins Soares <lucasmsoares96@gmail.com>
2025-06-15 16:03:23 -0300

    First Commit

---
+++ ironhide/settings.py
@@ -0,0 +1,23 @@
+"""Settings configuration for the Ironhide framework, including API endpoints, keys, models, and general options."""
+
+from pydantic_settings import BaseSettings
+
+from ironhide.utils import Provider
+
+
+class Settings(BaseSettings):
+    """Configuration settings for the Ironhide framework, including API endpoints, keys, models, and general options."""
+
+    # LLM
+    ironhide_provider_url: str | None = None
+    ironhide_provider: Provider = Provider.openai
+    ironhide_api_key: str = ""
+    ironhide_completions_model: str = "gpt-4o-mini"
+    ironhide_audio_to_text_model: str = "whisper-1"
+
+    # General
+    log_level: str = "INFO"
+    ironhide_request_timeout: int = 60
+
+
+settings = Settings()
